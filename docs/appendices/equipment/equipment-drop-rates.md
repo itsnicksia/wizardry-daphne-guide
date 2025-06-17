f2d05355c96212002f75f2f875bc6aee3ac0065c
TheAxolotl-gh <theaxolotl.gillflick@gmail.com>
2025-06-17 16:17:10 -0700

    Equipment drop rates

---
+++ docs/appendices/equipment/equipment-drop-rates.md
@@ -0,0 +1,5 @@
+# Equipment Junk and Drop Rates
+
+This table is generated from the [Wizardry Variants Junk Tracker (editable)](https://docs.google.com/spreadsheets/d/1XzlwOeuDjlFJ86zUrFtE2sO6J5AIdis0PM-nC7O0MQw/edit?gid=1608165252#gid=1608165252).
+
+<div id="equipment-drop-rates">Loading…</div>
---
+++ docs/js/render-equipment-drop-rates.js
@@ -0,0 +1,43 @@
+const equipmentSheet = {
+    url: "https://docs.google.com/spreadsheets/d/1XzlwOeuDjlFJ86zUrFtE2sO6J5AIdis0PM-nC7O0MQw/gviz/tq?tqx=out:csv&sheet=Drop%20Data",
+    containerElementId: "equipment-drop-rates",
+    columnRange: [0, 9]
+}
+
+document$.subscribe(() => {
+    buildTableFromSheet(equipmentSheet);
+});
+
+function buildTableFromSheet({containerElementId, url, columnRange}) {
+    const container = document.getElementById(containerElementId);
+
+    if (!container) {
+        return;
+    }
+
+    Papa.parse(
+        url,
+        {
+            download: true,
+            header: false,
+            complete: ({ data, meta }) => {
+                let html = [
+                    "<div class='md-typeset__scrollwrap'>",
+                    "<div class='md-typeset__table'>",
+                    "<table><thead><tr>"];
+                data.shift().slice(...columnRange).forEach(h => html.push(`<th>${h}</th>`));
+                html.push("</tr></thead><tbody>");
+
+                // rows
+                data.forEach((row, index) => {
+                    html.push('<tr>');
+                    row.slice(...columnRange).forEach(columnValue => html.push(`<td>${columnValue}</td>`));
+                    html.push('</tr>');
+                });
+
+                html.push("</tbody></table></div></div>");
+                container.innerHTML = html.join("");
+            }
+        }
+    );
+}
